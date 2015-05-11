6deffd09e707e46bc5b2dc298a8a90701f19fccb
Braulio Diez <braulio.diez@gmail.com>
2015-05-11 09:06:28 +0200

    First demo (all in one solution web api + front end)

---
+++ 00 Start/MyMovieWeb/MyMovieWeb/RestModel/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyMovieWeb.RestModel
+{
+    public class Movie
+    {
+        public string Title { get;set; }
+        public string Sinopsis { get; set; }
+        public string PosterUrl { get; set; }
+    }
+}
---
+++ 00 Start/MyMovieWeb/MyMovieWeb/app/Components/movies/movies.js
@@ -0,0 +1,25 @@
+﻿angular.module('myApp.movies', ['myApp.services']);
+
+(function () {
+    'use strict';
+
+    angular.module('myApp.movies')
+    .controller('MoviesController', ['moviesService', moviesController]);
+
+    function moviesController(moviesService) {
+        var vm = this;
+        vm.heading = "test movies heading";
+        vm.movies = [{title: "test title"}];
+
+        function initialize() {
+           
+
+            moviesService.getMovies().then(function (results) {
+                vm.movies = results.data;
+            }
+            );
+        }
+
+        initialize();            
+    }
+})();
