5e7b33075233163187a57ff4db1b8e83b3f42499
Braulio Diez <braulio.diez@gmail.com>
2015-05-24 12:18:39 +0200

    Completed webmin server plus fixes on concat / process and css

--- 04 Build/frontend/gruntfile.js
+++ 04 Build/frontend/gruntfile.js
@@ -24,16 +24,20 @@ module.exports = function(grunt) {
     ,concat: {
       dist: {
         files: {
-         './build/app.min.js': [
+         './build/app/app.min.js': [
             './src/app/app.js',
-            './src/gbnui/servicModule.js',
-            ['./src/app/components/**', '!./src/app/components/**/*.html'],            
+            './src/app/serviceModule.js',
+            ['./src/app/components/**', '!./src/app/components/**/*.html'],
+            ['./src/app/services/**', '!./src/app/services/**/*.html'],
           ],
-          './build/lib.min.js': [
+          './build/app/lib.min.js': [
             './src/lib/jquery/dist/jquery.min.js',
             './src/lib/bootstrap/dist/js/bootstrap.min.js',
             './src/lib/angularjs/angular.min.js',
-            './src/lib/angular-new-router/dist/router.es5.js',           
+            './src/lib/angular-new-router/dist/router.es5.min.js'                       
+          ],
+          './build/app/style.css': [
+            './src/lib/bootstrap/dist/css/bootstrap.css'
           ]
         }
       }
@@ -42,12 +46,12 @@ module.exports = function(grunt) {
     ,uglify: {
       build: {
         options: {
-          sourceMap: true,
-          sourceMapName: './build/app.min.js.map'
+          sourceMap: true//,
+          /*sourceMapName: './build/app/app.min.js.map'*/
         },        
         files: [
           {
-            './build/app.min.js' : './build/app.min.js'            
+            './build/app/app.min.js' : './build/app/app.min.js'            
           }
         ]              
       }        
@@ -63,7 +67,7 @@ module.exports = function(grunt) {
 
 
     ,connect: {
-      'static': { // This server will serve HTML + JS
+      'staticDev': { // This server will serve HTML + JS not minified
             options: {
                 hostname: 'localhost',
                 base: 'src',
@@ -72,7 +76,16 @@ module.exports = function(grunt) {
             }
         },
 
-        server: { // Proxy server, hub, will receive all request 
+      'staticMin': { // This server will serve HTML + JS minified
+            options: {
+                hostname: 'localhost',
+                base: 'build',
+                directory: 'build',            
+                port: 8091
+            }
+        },
+
+        serverDev: { // Proxy server, hub, will receive all request 
           options: {
             hostname: 'localhost',
             port: 8080,
@@ -97,12 +110,40 @@ module.exports = function(grunt) {
             }          
           ]
         }
+
+        ,serverMin: { // Proxy server, hub, will receive all request 
+          options: {
+            hostname: 'localhost',
+            port: 8090,
+            keepalive:true,
+            middleware: function(connect) {
+              return [proxySnippet];
+            },            
+            open: {
+              target: 'http://localhost:8090/index.html'
+            }
+          },
+          proxies: [ // Proxy configuration, if /api or /images request redirect to web api server
+            {        // else redirect to static server (html + js)
+                context: ['/api', '/images'],
+                host: 'localhost',
+                port: 10048
+            },
+            {
+                context: '/',
+                host: 'localhost',
+                port: 8091
+            }          
+          ]
+        }        
      },
   });
 
   grunt.registerTask('default', ['web']);
   
-  grunt.registerTask('web', ['connect:static', 'configureProxies:server', 'connect:server']);
+  grunt.registerTask('web', ['connect:staticDev', 'configureProxies:serverDev', 'connect:serverDev']);
+
+  grunt.registerTask('webmin', ['connect:staticMin', 'configureProxies:serverMin', 'connect:serverMin']);
 
   grunt.registerTask('build', ['clean', 'copy', 'concat', 'uglify:build', 'processhtml']);
 
